0f577eea2a994953a098bdf8d278ab307cd16a63
Niranjan Kumar <niranjan32@gmail.com>
2022-11-16 16:08:22 +0530

    removed console

--- src/components/Child.js
+++ src/components/Child.js
@@ -3,7 +3,7 @@ import React from 'react'
 let childData= 'HELLOOOO'
 function Child(props) {
 
-    console.log('childdata in child component ',childData )
+    // console.log('childdata in child component ',childData )
    return (
     <div>
         Child Component
