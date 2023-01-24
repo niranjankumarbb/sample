a9416dd5ef9f03cc5150289eb1689b7f12e3da79
Niranjan Kumar <niranjan32@gmail.com>
2023-01-24 12:51:20 +0530

    formatted Register.js

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,40 +1,45 @@
-import React, {useState} from 'react'
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import {Navigate} from 'react-router'
+import { Navigate } from "react-router";
 
-let status= false
+let status = false;
 
- function Register(props) {
-  const [status1, setStatus1]= useState(false)
-   let navigate= useNavigate()
+function Register(props) {
+  const [status1, setStatus1] = useState(false);
+  let navigate = useNavigate();
 
-    const handleSubmit= ()=>{
+  const handleSubmit = () => {
     //  navigate('/dashboard')
     // status=true;
     // console.log('status value- ', status)
-    localStorage.setItem('key', true)
+    localStorage.setItem("key", true);
 
-     setStatus1(prevStatus1=> !prevStatus1)
+    setStatus1((prevStatus1) => !prevStatus1);
 
     // setStatus1('true')
-    }
- 
-    return (
+  };
+
+  return (
     <div>
-         <h2> Login</h2>
-         <label> Email</label><br/>
-         <input type='text'/><br/><br/>
+      <h2> Login</h2>
+      <label> Email</label>
+      <br />
+      <input type="text" />
+      <br />
+      <br />
 
-         <label> Password</label><br/>
-         <input type='password'/><br/><br/>
+      <label> Password</label>
+      <br />
+      <input type="password" />
+      <br />
+      <br />
 
-        <button onClick={handleSubmit }>  Submit</button>
-         {status1?  <Navigate to ='/dashboard'/>  : <button>true</button>}
- 
-        {/* {status1 &&  <button>true</button> } */}
+      <button onClick={handleSubmit}> Submit</button>
+      {status1 ? <Navigate to="/dashboard" /> : <button>true</button>}
+
+      {/* {status1 &&  <button>true</button> } */}
     </div>
-  )
+  );
 }
 
-export default Register
- 
+export default Register;
